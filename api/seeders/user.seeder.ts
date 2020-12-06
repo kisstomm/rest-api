b2cd6db4c68f96cbb0eb0016fbbb81533062b0bb
Kiss Tamas <kisstomm@gmail.com>
2020-12-06 18:45:22 +0100

    add new row to seeder

--- api/seeders/user.seeder.ts
+++ api/seeders/user.seeder.ts
@@ -4,20 +4,17 @@ import  UserEntity  from '../entities/user.entity'
  
 export default class CreateUsers implements Seeder {
   public async run(_factory: Factory, connection: Connection): Promise<any> {
-    // await connection
-    //   .createQueryBuilder()
-    //   .insert()
-    //   .into(UserEntity)
-    //   .values([
-    //     { name: 'John Wick', birthYear: 1960 },
-    //   ])
-    //   .execute();
-
     await connection
-      .createEntityManager()
       .query(
         'INSERT INTO `users` (`name`, `birth_year`, `is_admin`) VALUES (?, ?, ?)',
-        ['John Wick', 1960, true],
-      );
-  }
+        ['John Wick', 1960, true]
+        );
+
+    await connection
+      .query(
+        'INSERT INTO `users` (`name`, `birth_year`, `is_admin`) VALUES (?, ?, ?)',
+        ['Clark Kent', 1938, false]
+        );
+      }
+      
 }
